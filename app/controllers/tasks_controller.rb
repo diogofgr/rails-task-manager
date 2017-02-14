1d004a3f5ab8d1ef2567aa20bd7b18f0bfe82d66
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-14 15:56:45 +0000

    Add private method in tasks controlller to make shit more DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,21 +4,32 @@ class TasksController < ApplicationController
   end
 
   def show
+    get_task
   end
 
   def new
   end
 
   def create
+    Task.create(description: params[:description])
+
+    redirect_to tasks_index_path
   end
 
   def edit
+    get_task
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def get_task
+    @task = Task.find(params[:id])
   end
 
 end
