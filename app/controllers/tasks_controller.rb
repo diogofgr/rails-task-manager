db71ae29a11df0d15d4ca9f80e81c5e4369ae041
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-14 16:56:38 +0000

    Add delete function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,10 +8,12 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+
   end
 
   def create
-    Task.create(description: params[:description])
+    Task.create(task_params)
 
     redirect_to tasks_index_path
   end
@@ -21,15 +23,28 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = get_task
+    @task.update(task_params)
+
+    redirect_to task_path
+
   end
 
   def destroy
+    @task = get_task
+    @task.destroy
+
+    redirect_to tasks_index_path
   end
 
   private
 
   def get_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:description, :done)
   end
 
 end
